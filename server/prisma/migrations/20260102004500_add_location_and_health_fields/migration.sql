36e5811856f6a059da6a9c9e046bf4508ff0e1d6
Ishmaam Iftekhar Khan <ishmaam@iut-dhaka.edu>
2026-01-02 00:47:35 +0600

    migration for location added

---
+++ server/prisma/migrations/20260102004500_add_location_and_health_fields/migration.sql
@@ -0,0 +1,7 @@
+-- AlterTable
+ALTER TABLE "UserProfile" ADD COLUMN "latitude" DOUBLE PRECISION,
+ADD COLUMN "longitude" DOUBLE PRECISION;
+
+-- AlterTable
+ALTER TABLE "FoodListing" ADD COLUMN "latitude" DOUBLE PRECISION,
+ADD COLUMN "longitude" DOUBLE PRECISION;
